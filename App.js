22ca87cbcb55ae7e0e76068ce897f541152b6c53
kendradalley <kendracdalley@gmail.com>
2016-11-14 14:34:10 -0800

    app component

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
 import React from 'react';
 // class component
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {txt: 'this is the state txt'}
+    this.update = this.update.bind(this)
+  }
+  update(event){
+    this.setState({txt: event.target.value})
+  }
+
   render(){
     return ( 
       <div>
-      <h1>Hello</h1>
-      <b>Bold</b>
+        <Widget txt= {this.state.txt} update={this.update} />
+
       </div>
       );
   }
 }
+
+// stateless component
+
+const Widget = (props) => {
+  return (
+  <div>
+   <input type= "text"
+        onChange={this.update}/>
+      <h1>{this.state.txt}</h1>
+  </div>
+  )
+}
+
 
 export default App
--- main.js
+++ main.js
@@ -1,4 +1,20 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import App from './App';
-ReactDom.render(<App />, document.getElementById('app'));
+ 
+ class App extends React.Component {
+  render(){
+    let txt = this.props.txt
+    return <h1>{txt}</h1>
+  }
+ }
+
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired
+}
+
+App.getDefaultProps = {
+  txt: 'this is the default txt'
+}
+
+ReactDom.render(<App cat = {5} />, document.getElementById('app'));
